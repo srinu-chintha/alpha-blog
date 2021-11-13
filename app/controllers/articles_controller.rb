e7fcdde0ec0357b859b13f0217fe10d129d0cc9d
srinu-chintha <chinthasrinu95745@gmail.com>
2021-11-13 17:57:39 +0530

    DRY

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController<ApplicationController
+    before_action :set_article,only: [:show,:edit,:update,:destroy]
     def show
-        @article=Article.find(params[:id])
     end
     def index
         @articles=Article.all
@@ -9,7 +9,7 @@ class ArticlesController<ApplicationController
         @article=Article.new
     end
     def create
-        @article=Article.new(params.require(:article).permit(:title,:description))
+        @article=Article.new(article_params)
         if @article.save
             #redirect_to article_path(@article.id)
             flash[:notice]="Article was created successfully"
@@ -19,11 +19,9 @@ class ArticlesController<ApplicationController
         end
     end
     def edit
-        @article=Article.find(params[:id])
     end
     def update
-        @article=Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title,:description))
+        if @article.update(article_params)
             flash[:notice]="Article was updated successfully"
             redirect_to @article
         else
@@ -32,9 +30,15 @@ class ArticlesController<ApplicationController
 
     end
     def destroy
-        @article=Article.find(params[:id])
         @article.destroy
         flash[:notice]="Article is deleted successfully"
         redirect_to articles_path
+    end
+    private
+    def set_article
+        @article=Article.find(params[:id])
+    end
+    def article_params
+        params.require(:article).permit(:title,:description)
     end
 end
