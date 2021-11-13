438f17152b09936e4daf938a4ef952268fa882fc
srinu-chintha <chinthasrinu95745@gmail.com>
2021-11-13 23:16:46 +0530

    added delete function

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article<ApplicationRecord
-    validates :title ,presence: true,length: {minimum: 3,maximum: 400}
-    validates :description,presence: true,length: {minimum: 3,maximum: 400}
+    validates :title ,presence: true,length: {minimum: 3,maximum: 4000}
+    validates :description,presence: true,length: {minimum: 3,maximum: 4000}
 end
