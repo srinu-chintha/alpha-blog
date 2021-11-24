83eea86c42d25c4e677bab1d8147021764ecdfce
srinu-chintha <chinthasrinu95745@gmail.com>
2021-11-24 22:06:09 +0530

    added bcrypt from backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User<ApplicationRecord
             uniqueness: {case_sensitive: false}
     validates :email,presence: true,length: {maximum: 25},
             uniqueness: {case_sensitive: false},format: {with: VALID_EMAIL}
+
+    has_secure_password
 end
